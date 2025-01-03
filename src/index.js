7844290f95967b1daf91135066570334929e6302
Bikash Mohanty <vikash070310@gmail.com>
2025-01-03 19:17:19 +0530

    updated package.json for prod

--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Auth0Provider
   domain="dev-77xfvtq4jkx78p2i.us.auth0.com"
-  clientId="MvYdG9otTbOpowJuPFdo2I58FTmht3x6"
+  clientId="H4YHXQ9vK1TlBBuUAsQbC5Fno7ZE96F1"
   authorizationParams={{
-    redirect_uri: "http://localhost:3000"
+    redirect_uri: "https://plato-education.com"
   }}
 >
   <App />
