f78e1e70d2e5970f2fd320dbd31e044b66770779
Bikash Mohanty <vikash070310@gmail.com>
2025-01-03 15:37:51 +0530

    hardcoded apiurl

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,11 @@
 // src/services/api.ts
 import axios from 'axios';
 
-const apiUrl = process.env.REACT_APP_API_URL;
-console.log('API URL:', apiUrl); // For debugging
+// const apiUrl = process.env.REACT_APP_API_URL;
+// console.log('API URL:', apiUrl); // For debugging
 
 const api = axios.create({
-  baseURL: apiUrl,
+  baseURL: "https://plato-backend-fzfaeqguehfaaahq.eastus2-01.azurewebsites.net",
   timeout: 20000,
   headers: {
     'Content-Type': 'application/json',
