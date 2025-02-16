eef88367696ff581be9f8caf0d8ce6c130930a1d
Bikash Mohanty <vikash070310@gmail.com>
2025-02-16 11:15:12 +0530

    final commit for prod

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { createRoot } from 'react-dom/client';
-import { Auth0Provider } from '@auth0/auth0-react';
 import { PostHogProvider } from 'posthog-js/react'
 
 const options = {
